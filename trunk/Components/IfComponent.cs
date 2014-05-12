efd24bb4162abf85eda4c2e1dbef6b724028dd47
roger.castaldo@gmail.com <roger.castaldo@gmail.com@c4f252cf-9b2b-3cd2-8c5e-a00c72c023ee>
2014-05-12 18:19:27 +0000

    added in an Else component wrt if sections {{#else#}} to indicate the else for the current if statement

--- trunk/Components/IfComponent.cs
+++ trunk/Components/IfComponent.cs
@@ -31,7 +31,7 @@ namespace Org.Reddragonit.MustacheDotNet.Components
                 foreach (IComponent child in _children)
                 {
                     ret++;
-                    if (child is IfComponent)
+                    if (child is IfComponent && child.Text!="#else#")
                         ret += ((IfComponent)child).Length;
                 }
                 return ret+1;
@@ -49,10 +49,26 @@ namespace Org.Reddragonit.MustacheDotNet.Components
             int num = int.Parse(_RegNum.Match(dataVariable).Value);
             dataVariable = dataVariable.Replace(num.ToString(), "");
             string subCode = "";
+            string elseCode = "";
+            bool inElse = false;
+            string varName = dataVariable + (_text.Substring(1).StartsWith("%") ? num : num + 1).ToString();
+            foreach (IComponent comp in _children)
+            {
+                if (comp.Text == "#else#")
+                {
+                    inElse = true;
+                }
+                else
+                {
+                    if (inElse)
+                        elseCode += comp.ToJSCode(varName);
+                    else
+                        subCode += comp.ToJSCode(varName);
+                }
+            }
             if (_text.Substring(1).StartsWith("%"))
             {
-                foreach (IComponent comp in _children)
-                    subCode += comp.ToJSCode(dataVariable + num.ToString());
+                
                 FunctionComponent fc = new FunctionComponent(_text.Substring(1));
                 ret = fc.ToJSCode(dataVariable + num.ToString());
                 ret = ret.Substring(5);
@@ -61,17 +77,16 @@ namespace Org.Reddragonit.MustacheDotNet.Components
 @"var tmp{3}={1});
 if ({0}(tmp{3}==undefined ? false : (tmp{3}==null ? false : tmp{3}))){{
 {2}
-}}",
+}}{4}",
    new object[]{(_text[0] == '#' ? "" : "!"),
    ret,
    subCode,
-   num});
+   num,
+   (elseCode=="" ? "" : "else{"+elseCode+"}")});
             }
             else
             {
-                foreach (IComponent comp in _children)
-                    subCode += comp.ToJSCode(dataVariable + (num + 1).ToString());
-                ret += string.Format(
+                ret = string.Format(
     @"var tmp{2}={1};
 if ({0}(tmp{2}==undefined ? false : (tmp{2}==null ? false : tmp{2}))){{
     if (Array.isArray(tmp{2})||(tmp{2}==undefined ? undefined : (tmp{2}==null ? undefined : tmp{2}.at))!=undefined){{
@@ -90,6 +105,27 @@ if ({0}(tmp{2}==undefined ? false : (tmp{2}==null ? false : tmp{2}))){{
        dataVariable+(num+1).ToString(),
        subCode
  });
+                if (elseCode != "")
+                {
+                    ret += string.Format(
+    @"else{{
+    if (Array.isArray(tmp{2})||(tmp{2}==undefined ? undefined : (tmp{2}==null ? undefined : tmp{2}.at))!=undefined){{
+        for(var x{2}=0;x{2}<tmp{2}.length;x{2}++){{
+            {3}=(tmp{2}.at==undefined ? tmp{2}[x{2}] : tmp{2}.at(x{2}));
+            {4}
+        }}
+    }}else{{
+        {3}=tmp{2};
+        {4}
+    }}
+}}", new object[]{
+       (_text[0]=='#' ? "" : "!"),
+       Utility.CreateVariableString(dataVariable+num.ToString(), _text.Substring(1)),
+       num,
+       dataVariable+(num+1).ToString(),
+       elseCode
+ });
+                }
             }
             return ret;
         }
