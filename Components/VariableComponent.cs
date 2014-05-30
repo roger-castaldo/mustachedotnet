2f4b130aaf2af901602de1413da709e80e661088
roger.castaldo@gmail.com <roger.castaldo@gmail.com@c4f252cf-9b2b-3cd2-8c5e-a00c72c023ee>
2014-05-30 18:16:51 +0000

    updated efficiency code by defining the pref function as a this.pref so that it will only get defined once on the first call for the function.

--- Components/VariableComponent.cs
+++ Components/VariableComponent.cs
@@ -57,27 +57,27 @@ namespace Org.Reddragonit.MustacheDotNet.Components
                 else
                 {
                     ret = string.Format(@"if (Array.isArray({0})){{
-    ret+=pref(({0}==undefined ? '' : {0}.join('{1}')));
+    ret+=this.pref(({0}==undefined ? '' : {0}.join('{1}')));
 }}", new object[] { var, repeater });
                     if (subVar != "")
                         ret += string.Format(@"else if (Array.isArray({0})){{
     for(var i=0;i<{0}.length;i++){{
-        ret+=pref({0}[i]==undefined ? undefined : ({0}[i].{1}!=undefined ? {0}[i].{1} : ({0}[i].get != undefined ? {0}[i].get('{1}') : undefined)))+(i+1<{0}.length ? '{2}' : ''));
+        ret+=this.pref({0}[i]==undefined ? undefined : ({0}[i].{1}!=undefined ? {0}[i].{1} : ({0}[i].get != undefined ? {0}[i].get('{1}') : undefined)))+(i+1<{0}.length ? '{2}' : ''));
     }}
 }}else if ({0}.at!=undefined){{
     for(var i=0;i<{0}.length;i++){{
-        ret+=pref(({0}.at(i)==undefined ? undefined : ({0}.at(i).{1}!=undefined ? {0}.at(i).{1} : ({0}.at(i).get != undefined ? {0}.at(i).get('{1}') : undefined)))+(i+1<{0}.length ? '{2}' : ''));
+        ret+=this.pref(({0}.at(i)==undefined ? undefined : ({0}.at(i).{1}!=undefined ? {0}.at(i).{1} : ({0}.at(i).get != undefined ? {0}.at(i).get('{1}') : undefined)))+(i+1<{0}.length ? '{2}' : ''));
     }}
 }}", new object[] { subVar, subVarField, repeater });
                     ret += string.Format(@"else{{
-    ret+=pref(({0}==undefined ? '' : ({0}==null ? '' : {0}.toString()+'{1}')));
+    ret+=this.pref(({0}==undefined ? '' : ({0}==null ? '' : {0}.toString()+'{1}')));
 }}", new object[] { var, repeater });
                 }
                 return ret;
             }else if (_text.StartsWith("& "))
                 return string.Format("ret+=({0}==undefined ? '' : {0}.toString());", Utility.CreateVariableString(dataVariable, _text.Substring(2)));
             else 
-                return string.Format(@"ret+=pref(({0}==undefined ? '' : {0}.toString()));",
+                return string.Format(@"ret+=this.pref(({0}==undefined ? '' : {0}.toString()));",
                 Utility.CreateVariableString(dataVariable, _text.Trim('}')));
         }
     }
