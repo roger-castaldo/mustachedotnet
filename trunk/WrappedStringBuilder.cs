7cd3379a631b5e4667944313ef6b061cf54fd5c0
roger.castaldo@gmail.com <roger.castaldo@gmail.com@c4f252cf-9b2b-3cd2-8c5e-a00c72c023ee>
2014-10-27 13:56:56 +0000

    tweaked some compression issues.

---
+++ trunk/WrappedStringBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Org.Reddragonit.MustacheDotNet
+{
+    internal class WrappedStringBuilder
+    {
+        private StringBuilder _sb;
+        private bool _minimize;
+
+        public WrappedStringBuilder(bool minimize)
+        {
+            _sb = new StringBuilder();
+            _minimize = minimize;
+        }
+
+        public string ToString()
+        {
+            return _sb.ToString();
+        }
+
+        public void AppendLine(string line)
+        {
+            if (_minimize)
+                _sb.Append(line);
+            else
+                _sb.AppendLine(line);
+        }
+
+        public void AppendFormat(string format, object par1, object par2)
+        {
+            _sb.AppendFormat(format, par1, par2);
+        }
+
+        public void Append(object obj)
+        {
+            _sb.Append(obj);
+        }
+    }
+}
